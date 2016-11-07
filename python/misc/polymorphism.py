b172579ffdc6c2f31be8919d682ec98524a53d23
bennywinefeld <benny.winefeld@gmail.com>
2016-11-06 18:45:43 -0800

    Added comment to polymorphism.py

--- python/misc/polymorphism.py
+++ python/misc/polymorphism.py
@@ -7,15 +7,16 @@ class Foo:
     return a+5
   def take(self, a):
     print "We're in function take() of class Foo"
+    # Now launch function run. If it was an instance of Goo (subclass)
+    # then we'll actually invoke run() of Goo  
     return self.run(a)
       
-     
+# Goo is a subclass of Foo     
 class Goo(Foo):
     def run(self, a):
         print "We're in function run() of class Goo"
         return a+8
       
-
 a = Goo("inst_of_Goo")
 print a.take(4)
             
